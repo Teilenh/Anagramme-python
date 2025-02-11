36119693b7032dd84b161be19f74e061c6e6e584
Teilenh <125691903+Teilenh@users.noreply.github.com>
2025-02-11 15:46:24 +0100

    Add files via upload

---
+++ BISO-Projet-programmation-Anagrame-LETECHEUR.py
@@ -0,0 +1,24 @@
+## ANAGRAMME
+## liste des fonction : 
+### 1. Choix des joueur 1/2 - return nombre, je ne passe pas d'information
+### 2. génération des lettre - return lettres et stock le mots d'origine, doit passé un nombre de lettre saisi par le joueur solo
+### 3. vérification - passage de la var. qui contient lettre, et var. contient le/les mots
+### 4. systéme de point - compte le nombre de mots, puis de lettre puis additionne
+### 
+
+
+# Fonction : 
+def NbrJoueur():
+    print("choix")
+
+def GenLettr(NbrLettr):
+    print("lettre")
+
+def Verif(Lttr, Mots):
+    print("vérif")
+
+def SysPoint():
+    print("point")
+# Variables :
+
+# Programme Principale : 
